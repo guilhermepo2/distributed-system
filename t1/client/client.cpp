ec041a6910535521668c1ed8f05f8cdbf35ce183
Guilherme Oliveira <guilherme.061@gmail.com>
2016-09-05 22:41:33 -0300

    Hardcoded Messages on the Client

--- t1/client/client.cpp
+++ t1/client/client.cpp
@@ -1,9 +1,9 @@
 /*
-** client.c -- a stream socket client demo
-*/
+  My HTTP CLient that uses a Distributed File System server
+ */
 
+#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -11,13 +11,9 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <iostream>
-
 #include <arpa/inet.h>
 
-#define PORT "8000" // the port client will be connecting to 
-
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 1024
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -25,81 +21,125 @@ void *get_in_addr(struct sockaddr *sa)
   if (sa->sa_family == AF_INET) {
     return &(((struct sockaddr_in*)sa)->sin_addr);
   }
-  
   return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-  int sockfd, numbytes;  
+  int sockfd, numbytes;
   char buf[MAXDATASIZE];
   struct addrinfo hints, *servinfo, *p;
   int rv;
   char s[INET6_ADDRSTRLEN];
+  std::string host, port;
   
-  if (argc != 2) {
-    fprintf(stderr,"usage: client hostname\n");
-    exit(1);
-  }
-  
+  // getting command line arguments, hostname and port
+  if(argc != 3)
+    {
+      std::cout << "client usage: client hostname port" << std::endl;
+      exit(1);
+    }
+  else
+    {
+      host = argv[1];
+      port = argv[2];
+      std::cout << "Connecting on: " << host << ":" << port << std::endl;
+    }
+
+  // doing all the cleanup thing
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  
-  if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
-  }
-  
-  // loop through all the results and connect to the first we can
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype,
-			 p->ai_protocol)) == -1) {
-      perror("client: socket");
-      continue;
+
+  if((rv = getaddrinfo(host.c_str(), port.c_str(), &hints, &servinfo)) != 0)
+    {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return 1;
     }
-    
-    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
-      perror("client: connect");
-      continue;
+  else
+    {
+      std::cout << "successful call: getaddrinfo()" << std::endl;
     }
-    
-    break;
-  }
-  
-  if (p == NULL) {
-    fprintf(stderr, "client: failed to connect\n");
-    return 2;
-  }
-  
+
+  // doing all that looping thing, but, we're not going to use sockopt and binding, instead we're going to connect to the server
+  for(p = servinfo; p != NULL; p = p->ai_next)
+    {
+      if((sockfd = socket(p->ai_family, p->ai_socktype,
+			  p->ai_protocol)) == -1)
+	{
+	  perror("CLIENT: socket");
+	  continue;
+	}
+      else
+	{
+	  std::cout << "successful call: socket()" << std::endl;
+	}
+
+      if(connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+	{
+	  close(sockfd);
+	  perror("CLIENT: Connection Error");
+	  continue;
+	}
+      else
+	{
+	  std::cout << "sucessful call: connect()" << std::endl;
+	}
+
+      break;
+    }
+
+  if(p == NULL)
+    {
+      fprintf(stderr, "CLIENT: failed to connect\n");
+      return 2;
+    }
+  else
+    {
+      std::cout << "CLIENT: Connected!" << std::endl;
+    }
+
   inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
 	    s, sizeof s);
-  printf("client: connecting to %s\n", s);
-  
-  freeaddrinfo(servinfo); // all done with this structure
-  
-  if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-    perror("recv");
-    exit(1);
-  }
-  buf[numbytes] = '\0';
-  printf("client: received '%s'\n",buf);
+  std::cout << "CLIENT: Connected to: " << s << std::endl;
 
-  // now im going to send my message
-  std::string msg = "hey there qt :)";
-  if (send(sockfd, msg.c_str(), msg.size(), 0) == -1)
+  freeaddrinfo(servinfo); // we're done with the structure
+
+  // at this point on the program we're connected with the server
+  // now we have to send and/or receive messages
+  
+  // first let's try receiving one, maybe the server sent us a welcome message, right?
+  /*
+    Actually I don't care I don't need a message
+    if((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1)
+    {
+    perror("CLIENT: Error at Receiving");
+    exit(1);
+    }
+    buf[numbytes] = '\0';
+    std::cout << "CLIENT: Received message: \n" << buf << std::endl;
+  */
+
+  // now we have to send our own message...
+  // first we have to get the message right?
+  // but how... ?
+  // who cares, let's just hardcode it.
+  
+  std::string post_example = "POST /obladi/oblada HTTP/1.1\nHost: 127.0.0.1\nContent-length: 10\n\n123456789\n\n";
+  std::string put_example = "PUT /obladi/oblada HTTP/1.1\nHost: 127.0.0.1\nContent-length: 10\n\n0987654321\n\n";
+  std::string get_example = "GET / HTTP/1.1\nHost: 127.0.0.1\n\n";
+  std::string another_get_example = "GET /obladi/oblada HTTP/1.1\nHost: 127.0.0.1\n\n";
+  // now send one of them!
+  // the message should be encoded?
+  if(send(sockfd, post_example.c_str(), post_example.size(), 0) == -1)
     perror("send");
-  std::cout << msg << " was sent to the server" << std::endl;
+  else
+    std::cout << "CLIENT: Message sent." << std::endl;
 
-  if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-    perror("recv");
-    exit(1);
-  }
-  buf[numbytes] = '\0';
-  printf("client: received '%s'\n",buf);
-  
+  // now we wait for the response and process it
+
   close(sockfd);
-  
   return 0;
 }
+
+
--- t1/server/server.cpp
+++ t1/server/server.cpp
@@ -1,7 +1,9 @@
 /*
-** server.c -- a stream socket server demo
-*/
+  My HTTP Server that Handles a Distributed File System
 
+ */
+
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,170 +16,205 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include <iostream>
 
-#define BACKLOG 10	 // how many pending connections queue will hold
+#define BACKLOG 10 // maximum pending connections
 
+// sigchld thing handler
+// also i dont know what this is tbh lads
 void sigchld_handler(int s)
 {
 	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+// function get in addr
+// in stands for INPUT i guess...
+// basically it gets if its a IPv4 or a IPv6 address
+void * get_in_addr(struct sockaddr * sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+  if(sa->sa_family == AF_INET)
+    {
+      return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(int argc, char * argv[])
 {
-
-  // getting port via command line
-  char * port;
+  // GETTING PORT VIA COMMAND LINE
+  std::string port;
   if(argc < 2)
     {
-      std::cout << "usage: ./server port" << std::endl;
+      std::cout << "usage: server port" << std::endl;
       exit(1);
     }
   else
     {
       port = argv[1];
-      std::cout << "port: " << port << std::endl;
+      std::cout << "Will listen on port: " << port << std::endl;
     }
-  
-  
-  int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+
+  // STUFF WE NEED FOR THE CONNECTION
+  int sockfd, new_fd;                // listen on sock_fd, new connections on new_fd
+  // ADDRINFO STRUCT is used to prep the socket address structure to subsequente use
   struct addrinfo hints, *servinfo, *p;
-  struct sockaddr_storage their_addr; // connector's address information
+  // SOCKADDR_STORAGE STRUCT holds IPv4 and IPv6
+  struct sockaddr_storage their_addr; // connector`s adress information
   socklen_t sin_size;
   struct sigaction sa;
-  int yes=1;
+  int yes = 1;
   char s[INET6_ADDRSTRLEN];
   int rv;
   
-  memset(&hints, 0, sizeof hints); // clean the struct, making sure its empty
-  hints.ai_family = AF_UNSPEC;     // dont care if IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // SOCK_STREAM = TCP
-  hints.ai_flags = AI_PASSIVE;     // use my IP
+  memset(&hints, 0, sizeof hints);   // clean the structure, making sure it's empty
+  hints.ai_family = AF_UNSPEC;       // we dont care if its IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM;   // its a TCP connection
+  hints.ai_flags = AI_PASSIVE;       // using my IP
 
-  // getaddrinfo() - prepare to launch!
-  // does all kind of good stuff for you
-  if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
-  }
-
-  // loop through all the results and bind to the first we can
-
-  // socket()
-  // feed the result of getaddrinfo() to socket()
-  // you use sockets for later system calls using the network
-  
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype,
-			 p->ai_protocol)) == -1) {
-      perror("server: socket");
-      continue;
+  // GETADDRINFO() - PREPARE TO LAUNCH!
+  if ((rv = getaddrinfo(NULL, port.c_str(), &hints, &servinfo)) != 0)
+    {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return 1;
+    }
+  else
+    {
+      std::cout << "successful call: getaddrinfo()" << std::endl;
     }
 
-    // if theres a socket remaining in the port (from previous use)
-    // please free it
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-		   sizeof(int)) == -1) {
-      perror("setsockopt");
-      exit(1);
-    }
-    
-    // bind()
-    // associate the socket with a local port
-    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
-      perror("server: bind");
-      continue;
-    }
-    
-    break;
-  }
+  // LOOPING THROUGH EVERY PIECE OF INFORMATION OBTAINED ON THE GETADDRINFO() AND BIND ON THE FIRST RESULT WE CAN
+  for(p = servinfo; p != NULL; p = p->ai_next)
+    {
+      if((sockfd = socket(p->ai_family, p->ai_socktype,
+			  p->ai_protocol)) == -1)
+	{
+	  perror("server: socket"); // could not bind to this one
+	  continue;
+	}
+      else
+	{
+	  std::cout << "successful call: socket() - got the socket descriptor" << std::endl;
+	}
 
-  // if p == null no socket could be binded
-  if (p == NULL)  {
-    fprintf(stderr, "server: failed to bind\n");
-    return 2;
-  }
+      // making sure there are not sockets leftovers on ports
+      if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+		    sizeof(int)) == -1)
+	{
+	  perror("setsockopt");
+	  exit(1);
+	}
+      else
+	{
+	  std::cout << "successful call: setsockopt() - cleaned port" << std::endl;
+	}
 
-  // free the linked list obtained in getaddrinfo()
-  // we already binded it, so who cares
+      // if there was a success, bind.
+      if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+	{ // if we could not bind
+	  close(sockfd);
+	  perror("error on bind");
+	  continue;
+	}
+      std::cout << "successful call: bind() - binded successfully" << std::endl;
+
+      break;
+    }
+
+  // if p is NULL we couldn't find any socket or couldn't bind one
+  if(p == NULL)
+    {
+      fprintf(stderr, "server: failed to bind\n");
+      return 2;
+    }
+
+  // don't need the addrinfo anymore, can free it
   freeaddrinfo(servinfo);
 
-  //Listen() Will Somebody please Call Me?
-  if (listen(sockfd, BACKLOG) == -1) {
-    perror("listen");
-    exit(1);
-  }
-  
-  sa.sa_handler = sigchld_handler; // reap all dead processes
+  // now we play the waiting game
+  // LISTEN() - WILL SOMEBODY PLEASE CALL ME?
+  if(listen(sockfd, BACKLOG) == -1)
+    {
+      perror("listen");
+      exit(1);
+    }
+  else
+    {
+      std::cout << "Listening on port: " << port << std::endl;
+    }
+
+  // sigaction thing
+  // i don't know what this is tbh fam
+  sa.sa_handler = sigchld_handler;
   sigemptyset(&sa.sa_mask);
   sa.sa_flags = SA_RESTART;
-  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
-  
-  printf("server: waiting for connections...\n");
-  
-  while(1) {  // main accept() loop
-    sin_size = sizeof their_addr;
-
-    // accept() - Thank you for calling
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    if (new_fd == -1) {
-      perror("accept");
-      continue;
+  if(sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+      perror("siaction");
+      exit(1);
+    }
+  else
+    {
+      std::cout << "successful call: sigaction()" << std::endl;
     }
 
-    // ntop = network to presentation
-    inet_ntop(their_addr.ss_family,
-	      get_in_addr((struct sockaddr *)&their_addr),
-	      s, sizeof s);
-    printf("server: got connection from %s\n", s);
-    
-    if (!fork()) { // this is the child process
-      close(sockfd); // child doesn't need the listener
+  std::cout << "SERVER ON!" << std::endl;
+  std::cout << "SERVER: Waiting for connections..." << std::endl;
 
-      /* ============================================ */
-      // CONNECTION ESTABILISHED HERE
-      std::string msg = "You are connected!";
-      if (send(new_fd, msg.c_str(), msg.size(), 0) == -1)
-	perror("send");
+  // everything set
+  // NOW we play the waiting game...
+  while(1)
+    { // main accept() loop
+      sin_size = sizeof their_addr;
 
-      //note: see that how the message is sent, it is created as as c++ string, but the send function requires a char * string, we can achieve that using the c_str() function on the c++ string, and also, to inform how many bytes are going over the network we have just to get the size from the c++ string
+      // ACCEPT() - Thank you for calling
+      new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+      if(new_fd == -1)
+	{
+	  perror("accept");
+	  continue;
+	}
 
-      // now we can listen to the client and send acks!
-      std::string ack = "ACK";
-      char buf[1024];
-      
-      int size;
-      // wait for a message, prints it and sends an ack
-      if(recv(new_fd, buf, 1024, 0) == -1)
-	perror("receive");
-      
-      std::cout << "received:" << buff << std::endl;
-      
-      if(send(new_fd, ack.c_str(), ack.size(), 0) == -1)
-	 perror("send");
-      /* ============================================ */
-      // GOODBYE CLIENT SEE YOU
-      close(new_fd);
-      exit(0);  // note that the CHILD process will end here
-                // the parent will still be running, so the server too
+      // ntop = network to presentation
+      // why am I doing this again?
+      inet_ntop(their_addr.ss_family,
+		get_in_addr((struct sockaddr *)&their_addr),
+		s, sizeof s);
+      std::cout << "SERVER: got connection from: " << s << std::endl;
+
+      if(!fork())
+	{ // the child process will handle the connection
+	  // NOW WE HANDLE EVERYTHING
+	  // we have to receive a HTTP Protocol Based message and so something
+	  // with it
+
+	  /*
+	    Why should the server send any message?
+
+	    std::string welcome = "<html>\n<body><h1>Hello World</h1></body>\n</html>";
+	    std::string welcome2 = "SERVER: You're now connected with the server, but I'm afraid I have to drop you :( sry mate";
+	    if(send(new_fd, welcome.c_str(), welcome.size(), 0) == -1)
+	    perror("send");
+	    else
+	    std::cout << "SERVER: greeting sent!" << std::endl;
+	  */
+
+	  
+	  // We get the requisition.
+	  char buffer[1024];
+	  int numbytes;
+	  if ((numbytes = recv(new_fd, buffer, 1023, 0)) == -1) {
+	    perror("recv");
+	    exit(1);
+	  }
+	  buffer[numbytes] = '\0';
+	  //std::cout << "SERVER: We received a message :^) This is the message: \n" << buffer << std::endl;
+	  std::cout << buffer << std::endl;
+	  // now we have the message, we should process it.
+	} // end of child process
+
+      close(new_fd); // the parent doesn't need the connection, it will just keep listening
     }
-    
-    close(new_fd);  // parent doesn't need this
-  }
-  
+
   return 0;
 }
+
