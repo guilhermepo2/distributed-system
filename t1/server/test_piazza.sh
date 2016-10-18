390465c6e851ae2fe0b3b573ca70dc309895f269
Guilherme Oliveira <guilherme.061@gmail.com>
2016-10-17 22:34:55 -0200

    piazza test

---
+++ t1/server/test_piazza.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+HOST=127.0.0.1
+PORT=8000
+
+curl --request POST \
+       	  --url http://$HOST:$PORT/arq1 \
+       	        --data 'açsldkfajsçdlfkajsd\nasldfkjasçdlfjkasdfkajsçdfkja\nlasdjfaçsldkfajçsdfk\nlaskdjfçasdkfjaçsdlkfajsçdlf\nalsdfjkaçsdfkajsdf\nasdf'
+
+
+for((x=0; x < 5000; x++));
+do
+   r=$RANDOM
+   r=$(( r %= 200 ))
+   curl --request PUT \
+       	  --url http://$HOST:$PORT/arq1 \
+       	        --data "asfasdfasdf$r" &
+done
+
+sleep 10
+echo ------------------------------------------------------
+
+curl --request GET --url http://$HOST:$PORT/arq1
